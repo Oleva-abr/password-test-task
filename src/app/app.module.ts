4f99c719706d1b92a6c5569ffaf70394603493fc
Olena <72007221+Oleva-abr@users.noreply.github.com>
2024-02-13 17:24:23 +0200

    create functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordFormComponent } from './password-form/password-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/password-form/password-form.component.ts
@@ -0,0 +1,51 @@
+import { Component, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-password-form',
+  templateUrl: './password-form.component.html',
+  styleUrls: ['./password-form.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordFormComponent),
+      multi: true
+    }
+  ]
+})
+export class PasswordFormComponent {
+  password: string = '';
+  passwordStrengthColor: string = 'gray';
+  passwordStrengthColor2: string = 'gray';
+  passwordStrengthColor3: string = 'gray';
+
+  calculateStrength() {
+    if (this.password.length === 0) {
+      this.passwordStrengthColor = 'gray';
+      this.passwordStrengthColor2 = 'gray';
+      this.passwordStrengthColor3 = 'gray';
+    } else if (this.password.length < 8) {
+      this.passwordStrengthColor = 'red';
+      this.passwordStrengthColor2 = 'red';
+      this.passwordStrengthColor3 = 'red';
+    } else {
+      const hasLetters = /[a-zA-Z]/.test(this.password);
+      const hasDigits = /\d/.test(this.password);
+      const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(this.password);
+
+      if ((hasLetters && hasDigits && !hasSymbols) || (hasLetters && !hasDigits && hasSymbols) || (!hasLetters && hasDigits && hasSymbols)) {
+        this.passwordStrengthColor = 'green';
+        this.passwordStrengthColor2 = 'green';
+        this.passwordStrengthColor3 = 'green';
+      } else if ((hasLetters && hasDigits) || (hasLetters && hasSymbols) || (hasDigits && hasSymbols)) {
+        this.passwordStrengthColor = 'yellow';
+        this.passwordStrengthColor2 = 'yellow';
+        this.passwordStrengthColor3 = 'gray';
+      } else {
+        this.passwordStrengthColor = 'red';
+        this.passwordStrengthColor2 = 'gray';
+        this.passwordStrengthColor3 = 'gray';
+      }
+    }
+  }
+}
